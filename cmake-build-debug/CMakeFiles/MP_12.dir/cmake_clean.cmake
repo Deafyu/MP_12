98acaf09eee61f8bfd9882694c9593fe21b01b96
deafyu <quuba68@wp.pl>
2020-06-04 17:07:06 +0200

    test

--- cmake-build-debug/CMakeFiles/MP_12.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/MP_12.dir/cmake_clean.cmake
@@ -1,9 +1,10 @@
 file(REMOVE_RECURSE
+  "CMakeFiles/MP_12.dir/libs/libClasses.cpp.obj"
   "CMakeFiles/MP_12.dir/main.cpp.obj"
   "MP_12.exe"
   "MP_12.exe.manifest"
-  "MP_12.lib"
   "MP_12.pdb"
+  "libMP_12.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
---
+++ libs/libClasses.h
@@ -0,0 +1,20 @@
+//
+// Created by knies on 04.06.2020.
+//
+
+#ifndef MP_12_LIBCLASSES_H
+#define MP_12_LIBCLASSES_H
+
+
+class CTable {
+
+};
+
+
+class CSortTable{
+    
+};
+
+
+
+#endif //MP_12_LIBCLASSES_H
