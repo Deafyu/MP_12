c89d5214cfff7c821a971b3f2e6eaeb8c440b32e
deafyu <quuba68@wp.pl>
2020-06-04 15:38:37 +0200

    test

---
+++ cmake-build-debug/CMakeFiles/MP_12.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MP_12.dir/main.cpp.obj"
+  "MP_12.exe"
+  "MP_12.exe.manifest"
+  "MP_12.pdb"
+  "libMP_12.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MP_12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
