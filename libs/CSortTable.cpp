62ab6d17b1283ec4d90e90309ff08a6a5b29e125
deafyu <quuba68@wp.pl>
2020-06-04 17:18:54 +0200

    Commit KN branch

--- libs/CSortTable.cpp
+++ libs/CSortTable.cpp
@@ -2,4 +2,4 @@
 // Created by knies on 04.06.2020.
 //
 
-#include "libClasses.h"
+#include "CSortTable.h"
---
+++ libs/CSortTable.h
@@ -0,0 +1,16 @@
+//
+// Created by knies on 04.06.2020.
+//
+
+#ifndef MP_12_CSORTTABLE_H
+#define MP_12_CSORTTABLE_H
+
+
+class CSortTable {
+
+};
+
+
+
+
+#endif //MP_12_CSORTTABLE_H
